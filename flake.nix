cd6f73aca7f74b5efbcac72abbbf2f310c63b7de
J. Ryan Stinnett <jryans@gmail.com>
2025-04-08 16:33:58 +0100

    Add Hugo to Nix flake

--- flake.nix
+++ flake.nix
@@ -2,10 +2,17 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
     systems.url = "github:nix-systems/default";
+    # Hugo 0.83.1
+    nixpkgs-hugo.url = "github:NixOS/nixpkgs/df23c968285f443cca67600e91a2724fa3166c34";
   };
 
   outputs =
-    { nixpkgs, systems, ... }:
+    {
+      nixpkgs,
+      systems,
+      nixpkgs-hugo,
+      ...
+    }:
     let
       forEachSystem =
         f:
@@ -15,15 +22,19 @@
             pkgs = import nixpkgs {
               inherit system;
             };
+            pkgs-hugo = import nixpkgs-hugo {
+              inherit system;
+            };
           }
         );
     in
     {
       devShells = forEachSystem (
-        { pkgs }:
+        { pkgs, pkgs-hugo }:
         {
           default = pkgs.mkShell {
             packages = with pkgs; [
+              pkgs-hugo.hugo
               nodejs_22
             ];
           };
